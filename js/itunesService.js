c37c18dc6bcb3af248347e390700c0478d058cf8
Daniel Baker <dbaker731@gmail.com>
2016-10-01 11:16:38 -0500

    finsishing the project

--- js/itunesService.js
+++ js/itunesService.js
@@ -4,9 +4,45 @@ angular.module('itunes').service('itunesService', function($http, $q){
 
   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
+  //Note that in the above line, artist is the parameter being passed in.
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
     //Code here
-    
+
+  var baseUrl = 'http://itunes.apple.com/search?term=';
+
+  this.getSongs = function( artist ){
+    var songArray = [];
+    var dfd = $q.defer();
+
+    $http({
+        method: 'JSONP',
+        url: baseUrl + artist + '&callback=JSON_CALLBACK'
+      }).then( function( results ){
+
+        for (var i = 0; i < results.data.results.length; i++) {
+          var song = {
+            AlbumArt: results.data.results[i].artworkUrl30,
+            Artist: results.data.results[i].artistName,
+            TrackName: results.data.results[i].trackName,
+            Collection: results.data.results[i].collectionCensoredName,
+            CollectionPrice: "$" + results.data.results[i].collectionPrice,
+            Play: results.data.results[i].previewUrl,
+            Type: results.data.results[i].kind,
+            SinglePrice: "$" + results.data.results[i].trackPrice
+          };
+          songArray.push(song);
+
+          if (results.data.results.length === songArray.length) {
+            dfd.resolve( songArray );
+          }
+        }
+
+
+      });
+
+      return dfd.promise;
+
+  };
+
 });
